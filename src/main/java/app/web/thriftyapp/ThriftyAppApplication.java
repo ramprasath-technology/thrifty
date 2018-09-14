79bd9d057d96e9d8b9d7d27d21f971065a3d93ee
Ramprasath Babu <ramprasath.technology@gmail.com>
2018-09-14 09:25:01 -0400

    initial project set up with customer POJO

---
+++ src/main/java/app/web/thriftyapp/ThriftyAppApplication.java
@@ -0,0 +1,12 @@
+package app.web.thriftyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThriftyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThriftyAppApplication.class, args);
+    }
+}
